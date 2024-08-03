392362db558aed9c5ee545d4771202ad27a95aed
jayarathinavel <jayrathinavel@gmail.com>
2024-08-03 14:18:49 +0530

    Health Page

--- health.php
+++ health.php
@@ -15,104 +15,107 @@
     </head>
     <body>
 
-        <header class="bg-success py-4">
-            <div class="container">
-                <div class="row d-flex align-items-center">
-                    <div class="col-md-6">
-                        <h4>Health</h4>
-                    </div>
-                    <div class="col-md-6 text-end">
-                        <a href="/" class="btn btn-light">Home</a>
-                    </div>
-                </div>
+    <nav class="navbar navbar-expand-lg bg-success navbar-dark p-2 mb-4">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Tools</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                </li>
+            </ul>
             </div>
-        </header>
-
-        <div class="container mt-5">
-            <h4 class="text-center">Health</h4>
-            <table class="table table-bordered table-responsive-md text-center">
-            <caption style="display:none">Health Table</caption>
-                <thead class="thead-dark">
-                    <tr>
-                        <th>Name</th>
-                        <th>Value</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    $versionValue = "N/A";
-                    $dbStatus = "Down";
-
-                    try {
-                        $conn = initDb();
-                        $query = "SELECT `value` FROM variables WHERE `key` = 'version'";
-                        $result = mysqli_query($conn, $query);
-                        if ($result->num_rows > 0) {
-                            $row = $result->fetch_assoc();
-                            $versionValue = $row['value'];
-                            $dbStatus = "Up";
-                        }
-                    } catch (Exception $e) {
-                        $error_message = $e->getMessage();
-                    }
-
-                    try {
-                        $smtpStatus = checkSMTPStatus(SMTP_SERVER, 587, SMTP_USERNAME, SMTP_PASSWORD);
-                    } catch (Exception $e) {
-                        $error_message = $error_message . "<br/>" . $e ->getMessage();
-                    }
-                    try {
-                        $urlToCheck = "youtube.com";
-                        $certificateInfo = checkCertificateValidity($urlToCheck);
-                    } catch(Exception $e) {
-                        $error_message = $error_message . "<br/>" . $e ->getMessage();
-                    }
-                    ?>
-                    <tr>
-                        <td>Version</td>
-                        <td>
-                            <?php echo $versionValue; ?>
-                        </td>
-                    </tr>
-                    <tr class="<?php echo ($dbStatus == 'Up') ? '' : 'table-danger'; ?>">
-                        <td>Database Status</td>
-                        <td>
-                            <?php echo $dbStatus; ?>
-                        </td>
-                    </tr>
-                    <tr class="<?php echo ($smtpStatus) ? '' : 'table-danger'; ?>">
-                        <td>Mail Server Status</td>
-                        <td>
-                            <?php echo ($smtpStatus) ? 'Up' : 'Down'; ?>
-                        </td>
-                    </tr>
-                    <tr class="display-none <?php echo ($certificateInfo['is_valid']) ? '' : 'table-danger'; ?>">
-                        <td>HTTPS Certificate Status</td>
-                        <td>
-                            <b>Status:</b> <?php echo ($certificateInfo['is_valid']) ? 'Valid' : 'Invalid'; ?> <br />
-                            <b>Subject:</b> <?php echo isset($certificateInfo['subject'])
-                                ? printAssociativeArray($certificateInfo['subject']) : 'N/A'; ?> <br />
-                            <b>Issuer:</b> <?php echo isset($certificateInfo['issuer'])
-                                ? printAssociativeArray($certificateInfo['issuer']) : 'N/A'; ?> <br />
-                            <b>Valid From:</b> <?php echo isset($certificateInfo['valid_from'])
-                                ? $certificateInfo['valid_from'] : 'N/A'; ?> <br />
-                            <b>Valid To:</b> <?php echo isset($certificateInfo['valid_to'])
-                                ? $certificateInfo['valid_from'] : 'N/A'; ?> <br />
-
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <?php if (!empty($error_message)): ?>
-                <div class="alert alert-danger" role="alert">
-                    <strong>Error:</strong> <br/>
-                    <?php echo $error_message; ?>
-                </div>
-            <?php endif; ?>
         </div>
-        <!-- Bootstrap JS and jQuery -->
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    </nav>
+
+    <div class="container mt-5">
+        <h4 class="text-center">Health</h4>
+        <table class="table table-bordered table-responsive-md text-center">
+        <caption style="display:none">Health Table</caption>
+            <thead class="thead-dark">
+                <tr>
+                    <th>Name</th>
+                    <th>Value</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $versionValue = "N/A";
+                $dbStatus = "Down";
+
+                try {
+                    $conn = initDb();
+                    $query = "SELECT `value` FROM variables WHERE `key` = 'version'";
+                    $result = mysqli_query($conn, $query);
+                    if ($result->num_rows > 0) {
+                        $row = $result->fetch_assoc();
+                        $versionValue = $row['value'];
+                        $dbStatus = "Up";
+                    }
+                } catch (Exception $e) {
+                    $error_message = $e->getMessage();
+                }
+
+                try {
+                    $smtpStatus = checkSMTPStatus(SMTP_SERVER, 587, SMTP_USERNAME, SMTP_PASSWORD);
+                } catch (Exception $e) {
+                    $error_message = $error_message . "<br/>" . $e ->getMessage();
+                }
+                try {
+                    $urlToCheck = "youtube.com";
+                    $certificateInfo = checkCertificateValidity($urlToCheck);
+                } catch(Exception $e) {
+                    $error_message = $error_message . "<br/>" . $e ->getMessage();
+                }
+                ?>
+                <tr>
+                    <td>Version</td>
+                    <td>
+                        <?php echo $versionValue; ?>
+                    </td>
+                </tr>
+                <tr class="<?php echo ($dbStatus == 'Up') ? '' : 'table-danger'; ?>">
+                    <td>Database Status</td>
+                    <td>
+                        <?php echo $dbStatus; ?>
+                    </td>
+                </tr>
+                <tr class="<?php echo ($smtpStatus) ? '' : 'table-danger'; ?>">
+                    <td>Mail Server Status</td>
+                    <td>
+                        <?php echo ($smtpStatus) ? 'Up' : 'Down'; ?>
+                    </td>
+                </tr>
+                <tr class="display-none <?php echo ($certificateInfo['is_valid']) ? '' : 'table-danger'; ?>">
+                    <td>HTTPS Certificate Status</td>
+                    <td>
+                        <b>Status:</b> <?php echo ($certificateInfo['is_valid']) ? 'Valid' : 'Invalid'; ?> <br />
+                        <b>Subject:</b> <?php echo isset($certificateInfo['subject'])
+                            ? printAssociativeArray($certificateInfo['subject']) : 'N/A'; ?> <br />
+                        <b>Issuer:</b> <?php echo isset($certificateInfo['issuer'])
+                            ? printAssociativeArray($certificateInfo['issuer']) : 'N/A'; ?> <br />
+                        <b>Valid From:</b> <?php echo isset($certificateInfo['valid_from'])
+                            ? $certificateInfo['valid_from'] : 'N/A'; ?> <br />
+                        <b>Valid To:</b> <?php echo isset($certificateInfo['valid_to'])
+                            ? $certificateInfo['valid_from'] : 'N/A'; ?> <br />
+
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <?php if (!empty($error_message)): ?>
+            <div class="alert alert-danger" role="alert">
+                <strong>Error:</strong> <br/>
+                <?php echo $error_message; ?>
+            </div>
+        <?php endif; ?>
+    </div>
+    <!-- Bootstrap JS and jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     </body>
 </html>
